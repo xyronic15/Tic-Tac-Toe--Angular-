6beb708da049790d88f964671fa703c310f5007c
Xyron Brual <xyronb@gmail.com>
2023-05-09 16:49:29 -0400

    completed main functionality

---
+++ tic-tac-toe/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    SquareComponent,
+    BoardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
